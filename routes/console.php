0a0ddf917b51b698f72b307bfb0d01d1275423fd
Hugo Suarez <hugo.suarez92@yahoo.com>
2026-05-08 10:20:14 -0400

    create artisan command for test email

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('mail {email}', function (string $email) {
+    Mail::raw('SMTP test from Refugio del Chucao.', function ($message) use ($email) {
+        $message->to($email)
+            ->subject('Refugio del Chucao mail test');
+    });
+
+    $this->info("Test email sent to {$email}");
+})->purpose('Send a temporary test email');
